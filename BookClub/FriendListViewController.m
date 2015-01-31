1012564bb3ae2ca861541b6ef707dd1cba8caefb
mewpre <yichin.sun@gmail.com>
2015-01-30 20:01:19 -0800

    Finished all requirements

--- BookClub/FriendListViewController.m
+++ BookClub/FriendListViewController.m
@@ -12,11 +12,14 @@
 #import "Person.h"
 #import "AppDelegate.h"
 
-@interface FriendListViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface FriendListViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 @property NSManagedObjectContext *context;
 @property NSArray *friendsList;
+@property NSArray *filteredFriendsArray;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+@property BOOL isAscending;
 
 @end
 
@@ -26,6 +29,7 @@
 {
     [super viewDidLoad];
     self.context = [AppDelegate appDelegate].managedObjectContext;
+    self.isAscending = NO;
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -34,30 +38,65 @@
     [self loadFriends];
 }
 
+- (void)sortFriends: (BOOL)isAscending
+{
+    NSSortDescriptor *sortByBookCount = [NSSortDescriptor sortDescriptorWithKey:@"suggestedBooks.@count" ascending:isAscending];
+    NSSortDescriptor *sortByName = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
+    NSArray *sortedFriends = [[self.user.friends allObjects] sortedArrayUsingDescriptors:@[sortByBookCount, sortByName]];
+    self.friendsList = sortedFriends;
+    [self.tableView reloadData];
+}
+
 - (void)loadFriends
 {
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:[Person description]];
     request.predicate = [NSPredicate predicateWithFormat:@"name == %@", @"User"];
     NSArray *userArray = [self.context executeFetchRequest:request error:nil];
     self.user = [userArray firstObject];
-    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
-    NSArray *sortedFriends = [[self.user.friends allObjects] sortedArrayUsingDescriptors:@[sortDescriptor]];
-    self.friendsList = sortedFriends;
-    [self.tableView reloadData];
+    [self sortFriends: self.isAscending];
 }
 
 #pragma mark - Table View Delegate Methods
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    Person *friend;
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    Person *friend = [self.friendsList objectAtIndex:indexPath.row];
+    if (self.searchBar.text.length != 0)
+    {
+        friend = [self.filteredFriendsArray objectAtIndex:indexPath.row];
+    }
+    else
+    {
+        friend = [self.friendsList objectAtIndex:indexPath.row];
+    }
     cell.textLabel.text = friend.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Number of suggested books: %lu", (unsigned long)friend.suggestedBooks.count];
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.friendsList.count;
+    if (self.searchBar.text.length != 0)
+    {
+        return self.filteredFriendsArray.count;
+    }
+    else
+    {
+        return self.friendsList.count;
+    }
+}
+
+- (IBAction)onSortButtonTapped:(id)sender
+{
+    self.isAscending = !self.isAscending;
+    [self sortFriends:self.isAscending];
+}
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name contains[c] %@",searchText];
+    self.filteredFriendsArray = [NSMutableArray arrayWithArray:[self.friendsList filteredArrayUsingPredicate:predicate]];
+    [self.tableView reloadData];
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -71,7 +110,15 @@
     {
         FriendDetailViewController *fvc = segue.destinationViewController;
         UITableViewCell *cell = sender;
-        Person *selectedFriend = [self.friendsList objectAtIndex:[self.tableView indexPathForCell:cell].row];
+        Person *selectedFriend;
+        if (self.searchBar.text.length != 0)
+        {
+            selectedFriend = [self.filteredFriendsArray objectAtIndex:[self.tableView indexPathForCell:cell].row];
+        }
+        else
+        {
+            selectedFriend = [self.friendsList objectAtIndex:[self.tableView indexPathForCell:cell].row];
+        }
         fvc.selectedFriend = selectedFriend;
     }
 }
